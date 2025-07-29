17fcad06690a0cdc661b9951f30dd9f9b76a2a1f
Ati <90914928+atibagheri@users.noreply.github.com>
2025-07-28 20:23:32 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,6 @@
 echo "🔁 Starting R Plumber API on port 8000..."
 /usr/bin/Rscript -e "pr <- plumber::plumb('backend/degviz_api/plumber.R'); pr$run(host='0.0.0.0', port=8000)" &
 
-echo "🌱 Activating Python virtual environment..."
-source backend/venv/bin/activate
-
 echo "🚀 Starting Flask app on port 5050..."
 cd backend
-python app.py
+python3 app.py
